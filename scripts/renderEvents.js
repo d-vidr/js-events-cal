5e411909e3e5028121d454339cf7306fd25eff9f
David Ramirez <dramirez@alleyinteractive.com>
2018-08-26 12:57:51 -0700

    refactor with js module design pattern

--- scripts/renderEvents.js
+++ scripts/renderEvents.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
  Assumptions:
  - I am not using ES6 since I'm not using a transpiler like Babel
@@ -9,26 +10,58 @@
   - Remove console.logs
 */
 
-var renderEvents = function(eventsArray) {
-  // Gatekeeper
-  if (!eventsArray || !Array.isArray(eventsArray)) {
-    return 'Please supply an array of events.';
-  }
-  var hourInPixels = 60;
-  var minuteInPixels = hourInPixels/60;
-  var dayWidthInPixels = 100;
-  var endTimes = [];
-  var eventWidth = 100;
-  
-  var calEventHeight = function(calEvent) {
-    return calEvent.duration * minuteInPixels;
-  }
+var calendarEvents = {
 
-  var calEventStartPixel = function(calEvent) {
-    return calEvent.starts_at * minuteInPixels;
-  }
+  // Project settings.
+  appNodeId: 'calendar',
+  startTime: 9,
+  endTime: 9,
+  hourInPixels: 60,
 
-  var compareStartTime = function(a,b) {
+  // Don't edit these.
+  minuteInPixels: this.hourInPixels/60,
+
+  // Events arrays.
+  calEvents: [],
+  sortedCalEvents: [],
+
+  // Error feedback.
+  errorMsg: '',
+
+  // HTML templates.
+  calEventTemplate: '<div class="cal-event" style="{$test}"></div>',
+
+  // Set up calendar.
+  setup: function(calEvents) {
+    this.calEvents = calEvents;
+  },
+
+  logError: function() {
+    if (this.errorMsg.length > 0) {
+      console.log(this.errorMsg);
+      this.debugLog();
+    }
+  },
+
+  debugLog: function() {
+    console.log(this);
+  },
+
+  // Check requirements.
+  requirementsMet: function() {
+    if (
+      !this.calEvents
+      || !Array.isArray(this.calEvents)
+      || 0 === this.calEvents.length
+      ) {
+        this.errorMsg = 'Please supply an array of events.';
+        return false;
+    }
+    return true;
+  },
+
+  // Compare function for sorting start times.
+  compareStartTime: function(a,b) {
     if (a.starts_at < b.starts_at) {
       return -1;
     }
@@ -36,50 +69,87 @@ var renderEvents = function(eventsArray) {
       return 1;
     }
     return 0;
-  }
+  },
 
-  var sortEvents = function() {
-    var eventsCopy = eventsArray;
-    return eventsCopy.sort(compareStartTime);
-  }
+  sortEvents: function() {
+    this.sortedCalEvents = this.calEvents.slice();
+    this.sortedCalEvents.sort(this.compareStartTime);
+  },
 
-  var renderEvent = function(calEvent) {
-    // Check existing end times against current start time and flush events that are over
-    console.log(calEvent);
-    console.log(endTimes);
-    if (endTimes.length > 0) {
-      endTimes.forEach(function(endTime) {
-        if (calEvent.starts_at > endTime) {
-          console.log('remove ' + endTime);
-          var indexToRemove = endTimes.indexOf(endTime);
-          // endTimes.splice(indexToRemove, 1);
-        }
-      })
-    }
+  /**
+   * Cycle through all sorted events and add overlap information.
+   * We will use 'overlapping_events' to calculate event width.
+   */
+  setOverlapInfo: function() {
+    const sortedCalEvents = this.sortedCalEvents;
+    let endTimes = [];
+    sortedCalEvents.forEach(function(calEvent) {
+      calEvent.overlaps = 0;
+      if (!calEvent.starts_at || !calEvent.duration) {
+        return; // Move on to next event.
+      }
+      if (endTimes.length > 0) {
+        let endTimesToRemove = [];
+        // Cycle through existing endTimes and check if current event starts after.
+        endTimes.forEach(function(endTime) {
+          if (calEvent.starts_at > endTime) {
+            endTimesToRemove.push(endTimes.indexOf(endTime));
+          }
+        });
+        // Remove irrelevant endTimes.
+        endTimesToRemove.forEach(function(endTime) {
+          endTimes.splice(endTimes.indexOf(endTime));
+        });
+      }
 
-    // If end times still exist, an event is still occurring
-    if (endTimes.length > 0) {
-      // There is an overlapping event
-      eventWidth = 100 / endTimes.length;
-      // console.log(calEvent);
-      // console.log('will overlap');
-    }
-    // console.log(endTimes);
-    // Push current end time to check against next event
-    endTimes.push(calEvent.starts_at + calEvent.duration);
-    
-  }
+      // If end times still exist, both current event and event in endTimes overlap.
+      if (endTimes.length > 0) {
+        const currentItemIndex = sortedCalEvents.indexOf(calEvent);
+        sortedCalEvents[currentItemIndex-1].overlaps += 1;
+        // Increment current calEvent overlaps by number of endTimes.
+        sortedCalEvents[currentItemIndex].overlaps = endTimes.length;
+      }
+      // Push current end time to check against next event.
+      endTimes.push(calEvent.starts_at + calEvent.duration);
+    })
+  },
 
-  // Sort events, then render them
-  sortedEvents = sortEvents();
-  sortedEvents.forEach(function(calEvent) {
-    // Ensure each event has required properties
-    if (!calEvent.starts_at || !calEvent.duration) {
-      console.log('Event does not have `starts_at` or `duration`, both of which are required to render');
+  renderEventMarkup: function(calEvent) {
+    const eventNode = document.createElement('div');
+    const overlaps = calEvent.overlaps;
+    const eventWidth = (overlaps > 0) ? 100/(overlaps+1) : 100;
+    eventNode.classList.add('calendar-item');
+    eventNode.setAttribute('style', `width: ${eventWidth}%;`);
+    eventNode.innerHTML = calEvent.starts_at;
+    return eventNode;
+  },
+
+  renderCalendarMarkup: function() {
+    const renderEventMarkup = this.renderEventMarkup;
+    const calendar = document.getElementById(this.appNodeId);
+    this.sortedCalEvents.forEach(function(calEvent) {
+      calendar.appendChild(renderEventMarkup(calEvent));
+    })
+  },
+
+  // Render calendar and events.
+  render: function(calEvents) {
+    this.setup(calEvents);
+    if (!this.requirementsMet()) {
+      this.logError();
       return;
     }
-    renderEvent(calEvent);
-  })
+    this.sortEvents();
+    this.setOverlapInfo();
+    this.renderCalendarMarkup();
+
+    // Logging for dev.
+    this.debugLog();
+  },
+};
+
+var renderEvents = function(calEvents) {
+  calendarEvents.render(calEvents);
 }
 
 // ***** FOR DEVELOPMENT ONLY *****
