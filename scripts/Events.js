0fabeabad718ef354b7c63f2183b3c2f63d55bf3
Dave Ramirez <dramirez@alley.co>
2019-06-02 22:19:00 -0700

    Set eventColTotals at end

--- scripts/Events.js
+++ scripts/Events.js
@@ -212,6 +212,14 @@ class Events {
       eventMap[position] = endTime;
       console.log(`eventMap[${position}]: ${endTime}`);
       this.simultaneousEventPositions[index] = position;
+
+      // If last item, set eventColTotals again
+      if (index === this.sortedEvents.length - 1) {
+        const cols = eventMap.length-1;
+        for (let idx = eventColTotalsOffset; idx <= index; idx++) {
+          this.eventColTotals[idx] = cols;
+        }
+      }
     })
   }
 
@@ -254,12 +262,26 @@ const renderEvents = function(events) {
  * How can we
  */
 const eventsArray = [
-  {starts_at: 120, duration: 45, title: "Meeting with Ben", location: "Coffee Shop"},
-  {starts_at: 135, duration: 215, title: "Another Meeting with Ben", location: "Coffee Shop by the tracks"},
-  {starts_at: 240, duration: 60, title: "Lunch with Karl", location: "TBA"},
-  {starts_at: 75, duration: 60, title: "Sync with John"},
+  {starts_at: 120, duration: 45, title: "1 Meeting with Ben", location: "Coffee Shop"},
+  {starts_at: 135, duration: 215, title: "2 Meeting with Ben", location: "Coffee Shop by the tracks"},
+  {starts_at: 240, duration: 60, title: "1 Lunch with Karl", location: "TBA"},
+  {starts_at: 75, duration: 60, title: "1 Sync with John"},
   {starts_at: 360, duration: 25},
-  {starts_at: 420, duration: 120}
+  {starts_at: 420, duration: 120},
+  {starts_at: 120, duration: 45, title: "3 Meeting with Ben", location: "Coffee Shop"},
+  {starts_at: 135, duration: 215, title: "4 Another Meeting with Ben", location: "Coffee Shop by the tracks"},
+  {starts_at: 240, duration: 60, title: "2 Lunch with Karl", location: "TBA"},
+  {starts_at: 75, duration: 60, title: "2 Sync with John"},
+  {starts_at: 360, duration: 25},
+  {starts_at: 420, duration: 120},
+  {starts_at: 220, duration: 45, title: "4 Meeting with Ben", location: "Coffee Shop"},
+  {starts_at: 35, duration: 115, title: "5 Another Meeting with Ben", location: "Coffee Shop by the tracks"},
+  {starts_at: 40, duration: 390, title: "3 Lunch with Karl", location: "TBA"},
+  {starts_at: 175, duration: 15, title: "3 Sync with John"},
+  {starts_at: 460, duration: 150},
+  {starts_at: 400, duration: 60},
+  {starts_at: 470, duration: 115, title: "6 Another Meeting with Ben", location: "Coffee Shop by the tracks"},
+  {starts_at: 470, duration: 390, title: "4 Lunch with Karl", location: "TBA"},
 ];
 
 // 120-165, 135-350, 240-300, 75-135, 360-385, 420-540
